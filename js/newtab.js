1c7c4fc915d07586d7d92c874a67d3c38670415e
lopezsarmiento <diego.lopezsarmiento@gmail.com>
2018-08-20 16:58:02 -0300

    Adding changes

--- js/newtab.js
+++ js/newtab.js
@@ -1,5 +1,5 @@
 // Object with images and locations
-var locations = [
+const locations = [
     {
         "id": 1,
         "name":"Cartagena - Colombia",
@@ -39,30 +39,20 @@ function getImage() {
 function startTime() {
     // DOM elements
     const clock = document.getElementById('clock');
+    const greetElement = document.getElementById('greet');
     // Clock vars
     const today = new Date();
     const h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
-    var greet;
     var name = "Pepper";
     m = checkTime(m);
     s = checkTime(s);
 
-    //Display time of day
-    if (h >= 00 && h <=12) {
-        greet = "Good morning, ";
-    } else if (h > 12 && h < 18) {
-        greet = "Good afternoon, ";
-    } else {
-        greet = "Good evening, ";
-    }
+    var greet = timeOfDay(h);
 
-    document.getElementById('greet').innerHTML = greet+name;
-    //Displaying time on screen
-    document.getElementById('clock').innerHTML = h + ":" + m;
-
-    var reloadTime = setTimeout(startTime, 500);
+    //Set greet value on screen
+    greetElement.innerHTML = `${greet}, ${name}`;
 
     // Add trailing zeros to mins
     m = checkTime(m);
@@ -72,6 +62,19 @@ function startTime() {
     const reloadTime = setInterval(startTime, 1000);
 }
 
+function timeOfDay(h) {
+    var greet;
+    //Display time of day
+    if (h >= 0 && h <=12) {
+        greet = "Good morning";
+    } else if (h > 12 && h < 18) {
+        greet = "Good afternoon";
+    } else {
+        greet = "Good evening";
+    }
+
+    return greet;
+}
 // add zero in front of numbers < 10
 function checkTime(i) {
     if (i < 10) {i = `0${i}`};  
@@ -80,25 +83,28 @@ function checkTime(i) {
 
 function checkWeather() {
 
-    var city = "buenos+aires";
-    var apiKey = "&appid=cf6f3902316f9fa78adcc4f336e2728a"; //5f008a01931300e3dc2c478c5095fd71
-    var units = "&units=metric";
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+units+apiKey;
+    const city = "buenos+aires";
+    const apiKey = "&appid=cf6f3902316f9fa78adcc4f336e2728a"; //5f008a01931300e3dc2c478c5095fd71
+    const units = "&units=metric";
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}${units}${apiKey}`;
+    // DOM Elements
+    const cityId = document.getElementById('city');
+    const tempId = document.getElementById('temp');
 
     // Create a request variable and assign a new XMLHttpRequest object to it.
-    var request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
 
     // Open a new connection, using the GET request on the URL endpoint
     request.open('GET', url, true);
 
     request.onload = function () {
         // Begin accessing JSON data here
-        var data = JSON.parse(this.response);
+        const data = JSON.parse(this.response);
 
         if (request.status >= 200 && request.status < 400) {
 
-            document.getElementById('city').innerHTML = data.name;
-            document.getElementById('temp').innerHTML = data.main.temp;
+            cityId.innerHTML = data.name;
+            tempId.innerHTML = data.main.temp;
 
         } else {
             console.log('error :' + 'request.status : ' + request.status);
